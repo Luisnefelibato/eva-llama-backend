18251db31b63fae1442ad5e2ad7a72f9c801a983
Luisnefelibato <luisfgomezosp@gmail.com>
2025-04-03 16:51:06 -0700

    Actualizacion de URLAPITUNNEL

--- eva_llama_14.py
+++ eva_llama_14.py
@@ -937,16 +937,16 @@ class GoogleCalendarIntegration:
 class OllamaClient:
     """Cliente para comunicarse con la API de Ollama."""
     
-    def __init__(self, model_name="llama3", api_url="https://huge-ads-smile.loca.lt/api/generate"):
+    def __init__(self, model_name="llama3", api_url="https://solid-ants-peel.loca.lt/api/generate"):
         """
         Inicializa el cliente de Ollama.
         
         Args:
             model_name: llama3
-            api_url:"https://huge-ads-smile.loca.lt/api/generate"
+            api_url:"https://solid-ants-peel.loca.lt/api/generate"
         """
         self.model_name = model_name
-        self.api_url = "https://huge-ads-smile.loca.lt/api/generate"
+        self.api_url = "https://solid-ants-peel.loca.lt/api/generate"
 
 
         self.check_connection()
